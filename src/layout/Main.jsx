ff3587bd47ca37071785b28463817057c033c555
OHshajim <ajshajimmax@gmail.com>
2024-05-18 13:27:35 +0600

    nav and footer add

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
+import Nav from "../Shared/Nav";
 
 const Main = () => {
     return (
         <div>
+            <Nav />
             <Outlet />
+            <Footer />
         </div>
     );
 };
