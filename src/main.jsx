cb277b9084b1ffbb90a6032d5a47c8c9319f1471
OHshajim <ajshajimmax@gmail.com>
2024-05-21 02:32:22 +0600

    footer add

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Routes from './Routes/Routes.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Routes} />
+    <HelmetProvider>
+      <RouterProvider router={Routes} />
+    </HelmetProvider>
   </React.StrictMode>,
 )
