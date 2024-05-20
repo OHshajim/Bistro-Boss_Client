736f2b8b6a0a8b875d7aa3655312db6e986e7b1a
OHshajim <ajshajimmax@gmail.com>
2024-05-20 20:55:12 +0600

    Blogs  add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import Banner from "./Banner";
+import Blogs from "./Blogs";
 import Categories from "./Categories";
 
 const Home = () => {
     return (
         <div className="space-y-20">
-            <Banner/>
+            <Banner />
             <div className="max-w-screen-2xl mx-auto space-y-20">
-            <Categories/>
-
+                <Categories />
+                <Blogs />
             </div>
         </div>
     );
