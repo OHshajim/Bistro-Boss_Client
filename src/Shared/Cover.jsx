775dd1729076d6e87141f2f966746e05c7f84d3a
OHshajim <ajshajimmax@gmail.com>
2024-05-21 03:40:33 +0600

    parallax use on  menu page

--- src/Shared/Cover.jsx
+++ src/Shared/Cover.jsx
@@ -1,3 +1,4 @@
+import { Parallax} from 'react-parallax';
 
 const Cover = ({ BGImage, subheading, heading, style }) => {
     const styles = {
@@ -7,12 +8,20 @@ const Cover = ({ BGImage, subheading, heading, style }) => {
 
     }
     return (
-        <div style={styles} className="hero h-[800px] p-28">
-            <div className=" text-center py-24 px-32 w-full text-white  bg-[#15151599] space-y-2">
-                <h3 className={` ${style?.heading ? style.heading : 'text-5xl'}`}>{heading}</h3>
-                <p className={` ${style?.subheading ? style.subheading : 'text-base font-semibold'}`}>{subheading}</p>
+        <Parallax
+            blur={{ min: -90, max: 70 }}
+            bgImage={BGImage}
+            bgImageAlt="food cover"
+            strength={300}
+        >
+            <div  className="hero h-[700px] p-28">
+                <div className=" text-center py-24 px-32 w-full text-white  bg-[#15151599] space-y-2">
+                    <h3 className={` ${style?.heading ? style.heading : 'text-5xl'}`}>{heading}</h3>
+                    <p className={` ${style?.subheading ? style.subheading : 'text-base font-semibold'}`}>{subheading}</p>
+                </div>
             </div>
-        </div>
+        </Parallax>
+
     );
 };
 
