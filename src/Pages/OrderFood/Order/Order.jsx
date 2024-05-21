30468694a5dfc9fd02f52d7dac6ec71cf7a0b22e
OHshajim <ajshajimmax@gmail.com>
2024-05-21 14:31:18 +0600

    tabs design set

--- src/Pages/OrderFood/Order/Order.jsx
+++ src/Pages/OrderFood/Order/Order.jsx
@@ -1,5 +1,10 @@
 import Cover from "../../../Shared/Cover";
 import OrderBG from '../../../assets/shop/banner2.jpg'
+
+// Tabs
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 const Order = () => {
     return (
         <div>
@@ -7,8 +12,23 @@ const Order = () => {
                 subheading={'Quick and Easy: Delicious Meals Delivered Right to Your Doorstep'}
                 heading={"Order Food"}
                 style={{ "heading": "text-8xl font-bold", 'subheading': "font-semiBold text-2xl" }} />
+            <Tabs className="my-20 max-w-screen-2xl mx-auto">
+                <TabList>
+                    <Tab>SALAD</Tab>
+                    <Tab>PIZZA</Tab>
+                    <Tab>SOUPS</Tab>
+                    <Tab>DESSERTS</Tab>
+                    <Tab>DRINKS</Tab>
+                </TabList>
 
-                
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+            </Tabs>
+
         </div>
     );
 };
--- src/Shared/Cover.jsx
+++ src/Shared/Cover.jsx
@@ -9,7 +9,7 @@ const Cover = ({ BGImage, subheading, heading, style }) => {
             strength={300}
         >
             <div  className="hero h-[750px] p-28">
-                <div className=" text-center py-24 px-32 w-full text-white  bg-[#15151599] space-y-2 max-w-6xl">
+                <div className=" text-center py-24 px-32 w-full text-white  bg-[#15151599] space-y-2 max-w-screen-2xl">
                     <h3 className={` ${style?.heading ? style.heading : 'text-5xl'}`}>{heading}</h3>
                     <p className={` ${style?.subheading ? style.subheading : 'text-base font-semibold'}`}>{subheading}</p>
                 </div>
