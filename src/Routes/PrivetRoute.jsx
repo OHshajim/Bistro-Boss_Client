c7542b0c18c7fd4656cdf30890386415f9a2ab8f
OHshajim <ajshajimmax@gmail.com>
2024-05-23 21:14:15 +0600

    privet route add

---
+++ src/Routes/PrivetRoute.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivetRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
+    if (loading) {
+        return <progress className="progress w-56"></progress>;
+    }
+    if (user) {
+        return (
+            { children }
+        );
+    }
+    return <Navigate to='/login' />
+};
+
+export default PrivetRoute;
