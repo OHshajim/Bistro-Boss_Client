0f5237ade827140cda7538ae5832655e114edcb4
OHshajim <ajshajimmax@gmail.com>
2024-05-29 20:48:18 +0600

    fixed a problem

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -7,7 +7,7 @@ const useAdmin = () => {
     const axiosSecure = useAxiosSecure()
     const { data: isAdmin, isPending: isAdminLoading } = useQuery({
         queryKey: [user?.email, 'isAdmin'],
-        enabled: !loading ,
+        enabled: !loading,
         queryFn: async () => {
             const res = await axiosSecure.get(`/user/admin/${user.email}`)
             console.log(res.data);
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-// import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
-    // const { logout } = useAuth()
-    // const navigate = useNavigate()
+    const { logout } = useAuth()
+    const navigate = useNavigate()
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access_token')
         // console.log("secured stopped", token);
@@ -20,14 +20,14 @@ const useAxiosSecure = () => {
     axiosSecure.interceptors.response.use(function (response) {
         return response;
     }, async (error) => {
-        // const status = error.response.status;
-        // console.log('status error in interceptor ', status);
+        const status = error.response.status;
+        console.log('status error in interceptor ', status);
 
         // for error action 
-        // if (status === 401 || status === 403) {
-        //     await logout();
-        //     navigate('/login')
-        // }
+        if (status === 401 || status === 403) {
+            await logout();
+            navigate('/login')
+        }
         return Promise.reject(error)
     })
     return axiosSecure;
